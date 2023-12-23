83e71ac1ee893be477c8bc169eaffca07ef4356c
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-23 14:13:07 -0800

    Initial Commit

---
+++ Uber Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uber Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uber Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-23.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
